09a919343e6f2449aa44c1c4e4c7ff828ebfe802
G1Ryslan1G <admik2009>
2020-02-18 14:52:48 +0300

    Начальный возврат модуля PaternBlazor

---
+++ PaternBlazor/Model/SQLLiteContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaternBlazor.Model
+{
+    public class SQLLiteContext : DbContext
+    {
+        public SQLLiteContext(DbContextOptions<SQLLiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
